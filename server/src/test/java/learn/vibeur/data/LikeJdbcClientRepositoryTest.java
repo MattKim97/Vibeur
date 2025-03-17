80ebc458a812e3bd6f27c1e6d485cf10055a9700
mattk <mkimucsb@gmail.com>
2025-03-17 15:27:30 -0700

    more indepths tests for likes

--- server/src/test/java/learn/vibeur/data/LikeJdbcClientRepositoryTest.java
+++ server/src/test/java/learn/vibeur/data/LikeJdbcClientRepositoryTest.java
@@ -1,11 +1,14 @@
 package learn.vibeur.data;
 
+import learn.vibeur.models.Like;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,17 +57,29 @@ class LikeJdbcClientRepositoryTest {
 
     @Test
     void shouldAddLike(){
+        List<Like> allLikes = repository.findAll();
+        assertEquals(3,allLikes.size());
         assertTrue(repository.addLike(1,1));
+        List<Like> allLikesAfter = repository.findAll();
+        assertEquals(4,allLikesAfter.size());
     }
 
     @Test
     void shouldRemoveLike(){
+        List<Like> allLikes = repository.findAll();
+        assertEquals(3,allLikes.size());
         assertTrue(repository.removeLike(1,3));
+        List<Like> allLikesAfter = repository.findAll();
+        assertEquals(2,allLikesAfter.size());
     }
 
     @Test
     void shouldNotRemoveLike(){
+        List<Like> allLikes = repository.findAll();
+        assertEquals(3,allLikes.size());
         assertFalse(repository.removeLike(1,1));
+        List<Like> allLikesAfter = repository.findAll();
+        assertEquals(3,allLikesAfter.size());
     }
 
     @Test
